56a3d1f4cfd866fea9b4f4ee165fb8a8cdc7cfc7
Joaopedromata <joaopmata182@gmail.com>
2020-10-20 16:53:56 -0300

    trying to sum quantity

--- src/controllers/Storage/ResultController.js
+++ src/controllers/Storage/ResultController.js
@@ -1,31 +1,56 @@
 const { where } = require('sequelize')
 const Input = require('../../models/Inputs')
+const { sum } = require('../../models/Inputs')
 const Inputs = require('../../models/Inputs')
 const Location = require('../../models/Location')
 const PlanCon = require('../../models/PlanCon')
+const sequelize = require('sequelize')
 
 module.exports = {
     
     async showStorageByLocations(req, res){
         
         const { product_id, location_id } = req.params
-        
-        // const sumPlancon = Inputs.sum('quantity', {
-        //     where: { product_id }
-        // }).then(sum => {
-        //     return res.json(sum)
+
+        // const result = await Location.findByPk(location_id,{ 
+        //     include: [
+        //         {
+        //             association: 'rms',
+        //             // association: 'rms',
+        //             include: [
+        //                 { 
+        //                     association: 'inputs',
+        //                     // where: { product_id },
+        //                     group: 'product_id'
+        //                 },
+        //             ]
+        //         },
+                
+        //     ]
         // })
 
-        const teste = await Location.findByPk(location_id,{ 
-            include: [
-                {
-                    association: 'plancons',
-                    include: [{ association: 'outputs' }]
-                }
-            ]
-        })
-
-        res.json(teste)
         
+
+        // return res.status(200).json(result)
+        
+        // const show = await Inputs.findAll({ 
+        //     // attributes: [
+                
+        //     //     sequelize.fn('sum', sequelize.col('quantity'))
+        //     // ]
+        // })
+
+        // const show = await Inputs.sum('quantity', {
+        //     group: 'product_id',
+          
+        //     where: {rm_id: 185}
+        // })
+
+        const show = await Inputs.findAll({ 
+            //group: 'product_id', 
+            attributes: ['product_id', [sequelize.fn('sum', sequelize.col('quantity')), 'total'], 'product_id']
+            })
+        return res.json(show)
+
     }
 }
